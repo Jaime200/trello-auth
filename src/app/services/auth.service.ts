ed04cc5fd6501ff7938c9c88a420031a8d69b15d
jaime200 <jaime.munoz200@gmail.com>
2024-06-09 00:39:09 -0600

    reistro de usuarios

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,18 @@ export class AuthService {
 
   login(email:string, password:string){
     return this.http.post(`${environment.API_URL}/api/v1/auth/login`, { email, password })
+  }
+
+  register(name:string, password:string, email:string){
+    return this.http.post(`${environment.API_URL}/api/v1/auth/register`, {name, email, password })    
+    .pipe(
+      catchError((error) => {
+        //console.log("errror from service ", error)
+        if (error.error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+          return throwError('El usuario ya existe');
+        }
+        return throwError('Ups algo salio mal');
+      })
+    )
   }
 }
