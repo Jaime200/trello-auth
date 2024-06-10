62a0ecd3b8ec3753bf2d5860c779586c71e6e803
jaime200 <jaime.munoz200@gmail.com>
2024-06-09 23:16:02 -0600

    implementando Redirect guard

---
+++ src/app/guards/redirect.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {  CanActivate, Router} from '@angular/router';
+import { TokenService } from '@services/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RedirectGuard implements CanActivate {
+
+  constructor(
+    private tokenService:TokenService,
+    private router:Router
+  ){
+
+  }
+  
+  canActivate() : boolean
+    {
+    const token = this.tokenService.getToken()
+    if(token) {
+      this.router.navigate(['/app/boards'])
+      return true
+    }
+
+    return false
+      
+  }
+  
+}
